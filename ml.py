9795fdab5703d8b5295ede1352e56e80a2f69ca4
MasterYiBot <willamhaye7788@gmail.com>
2017-01-13 14:09:57 +0800

    started

---
+++ ml.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 10:10:41 2017
+
+@author: wesst
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset
+
+dataset = pd.read_csv('Data.csv')
+print(dataset)
